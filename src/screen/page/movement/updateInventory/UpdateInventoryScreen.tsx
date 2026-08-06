3fdfcea699e094b80d2f4050a5958478d29bac25
dw.office <dimaswahyu.official@gmail.com>
2026-08-06 11:22:42 +0700

    (FEAT) add completed filter

--- src/screen/page/movement/updateInventory/UpdateInventoryScreen.tsx
+++ src/screen/page/movement/updateInventory/UpdateInventoryScreen.tsx
@@ -28,7 +28,7 @@ const STATUS_OPTIONS = [
   // { label: 'Inspection', value: 'PENDING_INSPECTION' },
   { label: 'Approved', value: 'APPROVED' },
   // { label: 'Rejected', value: 'REJECTED' },
-  // { label: 'Completed', value: 'COMPLETED' },
+  { label: 'Completed', value: 'COMPLETED' },
   // { label: 'Cancelled', value: 'CANCELLED' },
 ];
 
